ef02093c90216eae9de2d81bf3208953f1be8bef
LonelyTree <alexhugh121@gmail.com>
2020-07-03 16:13:56 -0700

    modifying for one page app

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid } from '@material-ui/core'
 import { Header } from '../Header/Header'
 import { useStyles } from './styles'
@@ -6,11 +6,12 @@ import { Nav } from '../Nav/Nav'
 
 export const Main = () => {
   const classes = useStyles()
+  const [page, setPage] = useState(null)
   return (
     <Grid container className={classes.main_container}>
       <Header />
-      <Nav />
-      <Grid item>DESCRIPTION</Grid>
+      <Nav lift={setPage} />
+      <Grid item>{(page === 0) ? <p>0</p> : (page === 1) ? <p>1</p> : (page === 2) ? <p>2</p> : null}</Grid>
       <Grid item>FOOTER</Grid>
     </Grid>
   )
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,16 +3,19 @@ import { Grid, BottomNavigation, BottomNavigationAction } from '@material-ui/cor
 // import { Link } from 'react-router-dom'
 import { useStyles } from './styles'
 
-export const Nav = () => {
+export const Nav = (props) => {
   const classes = useStyles()
   const [value, setValue] = useState(null)
-
+  const liftUp = (props) => {
+    props.lift(value)
+  }
   return (
     <Grid item className={classes.nav_container}>
       <BottomNavigation
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue)
+          liftUp()
         }}
         showLabels
         className={classes.root}
