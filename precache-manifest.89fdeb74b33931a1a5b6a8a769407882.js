f6593d8dc8e91717597830f63b78037021b3a0f3
LonelyTree <alexhugh121@gmail.com>
2020-07-19 12:45:28 -0700

    Updates

---
+++ precache-manifest.89fdeb74b33931a1a5b6a8a769407882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349f677260e17fad08499f220039f110",
+    "url": "/EcosRegulatory/index.html"
+  },
+  {
+    "revision": "d92d44f4734850603d80",
+    "url": "/EcosRegulatory/static/js/2.f4100d10.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/EcosRegulatory/static/js/2.f4100d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812e13456335cf1f62bf",
+    "url": "/EcosRegulatory/static/js/main.bf2f4fad.chunk.js"
+  },
+  {
+    "revision": "04b76f024031731b8568",
+    "url": "/EcosRegulatory/static/js/runtime-main.86b5fc49.js"
+  },
+  {
+    "revision": "0f81c76038756f918a4c04a4f4544c38",
+    "url": "/EcosRegulatory/static/media/Ecos_color_simple.0f81c760.png"
+  }
+]);
